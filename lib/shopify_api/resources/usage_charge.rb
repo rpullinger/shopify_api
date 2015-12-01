49dad780fbb9bf178fc2640eef6ddd94fbd3f8b6
Jason Zhao <jason.zhao@shopify.com>
2015-12-01 16:45:53 -0500

    usage charge all

--- lib/shopify_api/resources/usage_charge.rb
+++ lib/shopify_api/resources/usage_charge.rb
@@ -1,5 +1,11 @@
 module ShopifyAPI
   class UsageCharge < Base
     init_prefix :recurring_application_charge
+
+    class << self
+      def index(recurring_application_charge_id)
+        UsageCharge.find(:all, :params => { :recurring_application_charge_id => recurring_application_charge_id })
+      end
+    end
   end
 end
