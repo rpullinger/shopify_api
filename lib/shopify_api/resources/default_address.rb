b549ff844f1136f01e6617974bce9a6dd09ce26b
Richard Pullinger <r_pullinger@hotmail.com>
2015-12-14 00:15:31 +0000

    Create default_address.rb

---
+++ lib/shopify_api/resources/default_address.rb
@@ -0,0 +1,13 @@
+module ShopifyAPI
+  class DefaultAddress < Base
+
+    def enable
+      load_attributes_from_response(post(:enable))
+    end
+
+    def disable
+      load_attributes_from_response(post(:disable))
+    end
+
+  end
+end
